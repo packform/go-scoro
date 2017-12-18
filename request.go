ad7a3403771d3cdb8bcd959e04973ed686151f77
Victor Ladochkin <victor.ladochkin@onlite.net>
2017-12-18 22:50:44 +0700

    Add basic documaentation for Request functionality

--- request.go
+++ request.go
@@ -9,6 +9,16 @@ import (
 	resty "gopkg.in/resty.v1"
 )
 
+// Request helps to build and send custom request to Scoro API. It supports
+// automatic mappings of data structures into request body and from response.
+//
+// It is unlikely that you will need to use this type directly. Most of the time
+// service wrappers should be used instead. But it could be helpful
+// in some case, for instance, if some new request isn't implemented yet.
+//
+// Example:
+//
+// response := NewRequest(credentials).SetResponse(ProductResponse{}).View(id)
 type Request struct {
 	credentials Credentials
 	lang        string
@@ -16,6 +26,15 @@ type Request struct {
 	entityType  string
 }
 
+// ResponseHeader represents base part of API response that is common for
+// all responses.
+//
+// Example of defining new response type:
+//
+//		type SomeNewResponse{
+//			RequestHeader `json:",inline"`
+//			Data 					SomeDataType `json:"data,omitempty"`
+//	 	}
 type ResponseHeader struct {
 	Status     string `json:"status"`
 	StatusCode string `json:"statusCode"`
@@ -24,10 +43,18 @@ type ResponseHeader struct {
 	} `json:"messages,omitempty"`
 }
 
+// ResponseType interface provides methods for retrieving information common for all
+// responses. Each response type must implement this interface in order to be
+// compatible with Request type.
 type ResponseType interface {
 	GetResponseHeader() ResponseHeader
 }
 
+// NewRequest creates request object configured with specified credentials and
+// pointed to the endpoint corresponding to the specified entityType.
+//
+// entityType can be "products", "orders", "invoices" or any other type supported
+// by Scoro API
 func NewRequest(credentials Credentials, entityType string) Request {
 	return Request{
 		credentials: credentials,
@@ -36,11 +63,21 @@ func NewRequest(credentials Credentials, entityType string) Request {
 	}
 }
 
+// SetResponse method is to register the response object for automatic unmarshalling
+// of JSON responses. Response type shoul conforms to the ResponseType interface.
+//
+//		request.SetResult(ProductResponse{})
+//
+// Accessing a result value
+//		if resp, err := request.View(id); err != nil {
+//			product := resp.(*productResponse).Product
+//		}
 func (t Request) SetResponse(response ResponseType) Request {
 	t.respType = response
 	return t
 }
 
+// View method sends "view" action request
 func (t Request) View(id string) (interface{}, error) {
 	url := makeUrl(t.credentials.CompanyID, t.entityType, "view", id)
 	body := requestBody{Credentials: t.credentials, Lang: t.lang}
@@ -48,6 +85,7 @@ func (t Request) View(id string) (interface{}, error) {
 	return sendRequest(url, body, t.respType)
 }
 
+// List method sends "list" action request
 func (t Request) List(filter interface{}, page int, count int) (interface{}, error) {
 	url := makeUrl(t.credentials.CompanyID, t.entityType, "list")
 	body := requestBody{
@@ -61,6 +99,7 @@ func (t Request) List(filter interface{}, page int, count int) (interface{}, err
 	return sendRequest(url, body, t.respType)
 }
 
+// Modify method sends "modify" action request
 func (t Request) Modify(obj interface{}) (interface{}, error) {
 	url := makeUrl(t.credentials.CompanyID, t.entityType, "modify")
 	body := requestBody{Credentials: t.credentials, Lang: t.lang, Request: obj}
@@ -68,6 +107,7 @@ func (t Request) Modify(obj interface{}) (interface{}, error) {
 	return sendRequest(url, body, t.respType)
 }
 
+// Delete method sends "delete" action request
 func (t Request) Delete(filter interface{}) (interface{}, error) {
 	url := makeUrl(t.credentials.CompanyID, t.entityType, "delete")
 	body := requestBody{Credentials: t.credentials, Lang: t.lang, Request: filter}
