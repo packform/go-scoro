3141eb6c9efdd11f2ea93533c51b2dbbc56eb82a
Victor Ladochkin <victor.ladochkin@onlite.net>
2017-12-19 20:22:04 +0700

    Add contacts API examples

---
+++ examples/contacts/contacts.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"time"
+
+	scoro "github.com/lxmx/go-scoro"
+)
+
+func main() {
+	company := flag.String("company", "", "Company id")
+	apiKey := flag.String("api_key", "", "Scoro API key")
+	flag.Parse()
+
+	if *company == "" || *apiKey == "" {
+		fmt.Println("Please specify company and api_key")
+		return
+	}
+
+	credentials := scoro.Credentials{ApiKey: *apiKey, CompanyID: *company}
+
+	fmt.Println("List contacts: ")
+	listContacts(credentials)
+
+	fmt.Println("Create contact: ")
+	contact := createContact(credentials)
+
+	fmt.Println("Modify contact: ")
+	contact = modifyContact(credentials, contact)
+
+	fmt.Println("Remove contact: ")
+	removeContact(credentials, contact)
+}
+
+func listContacts(credentials scoro.Credentials) {
+	contacts, err := scoro.Contacts(credentials).List(nil, 0, 3)
+	if err != nil {
+		panic(err)
+	}
+
+	printObject(contacts)
+}
+
+func createContact(credentials scoro.Credentials) scoro.Contact {
+	contact := scoro.Contact{
+		Name:         "Viktor",
+		Lastname:     "Ladochkin",
+		Birthday:     scoro.Date{Time: time.Date(1984, 4, 3, 0, 0, 0, 0, time.UTC)},
+		ContactType:  "person",
+		IsClient:     scoro.Bool{Value: true},
+		ModifiedDate: scoro.Time{Time: time.Now()},
+		Sex:          "M",
+	}
+
+	result, err := scoro.Contacts(credentials).Modify(contact)
+	if err != nil {
+		panic(err)
+	}
+
+	printObject(result)
+	return *result
+}
+
+func modifyContact(credentials scoro.Credentials, contact scoro.Contact) scoro.Contact {
+	contact.Addresses = []scoro.Address{
+		scoro.Address{
+			City:    "Tomsk",
+			Country: "Russia",
+		},
+	}
+
+	result, err := scoro.Contacts(credentials).Modify(contact)
+	if err != nil {
+		panic(err)
+	}
+
+	printObject(result)
+	return *result
+}
+
+func removeContact(credentials scoro.Credentials, contact scoro.Contact) {
+	err := scoro.Contacts(credentials).Delete(*contact.ContactID)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("OK!")
+}
+
+func printObject(obj interface{}) {
+	jsObj, _ := json.MarshalIndent(obj, "", "\t")
+	fmt.Println(string(jsObj))
+}
